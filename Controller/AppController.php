4c2200502095d164e3b9e1e43f1dccc7eaf80349
WebAndCow <WebAndCow@users.noreply.github.com>
2013-12-05 16:41:32 +0100

    Missing HtmlHelper

--- Controller/AppController.php
+++ Controller/AppController.php
@@ -33,7 +33,7 @@ App::uses('Controller', 'Controller');
  */
 class AppController extends Controller {
 
-	public $helpers = array('Form', 'Bs', 'BsForm');
+	public $helpers = array('Html', 'Form', 'Bs', 'BsForm');
 
 	public $titre = 'Title';
 	public $description = '';
